dbe3cf270ebae03dd8504866ffd9edb09fac8a3d
Techno <meroypaulvincent@gmail.com>
2023-09-22 09:52:10 +0800

    realigned burger menu

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 const BURGER_MENU = document.getElementById('burger-menu')
+const MENU_TOGGLE = document.querySelectorAll(".toggle")
 
-BURGER_MENU.addEventListener("click", () => {
-    BURGER_MENU.classList.toggle("active")
-    BURGER_MENU.classList.toggle("inactive")
+MENU_TOGGLE.forEach(toggle => {
+    toggle.addEventListener("click", (e) => {
+        console.log(toggle, e.currentTarget)
+
+        BURGER_MENU.classList.toggle("active")
+        BURGER_MENU.classList.toggle("inactive")
+    })
 })
+
+// MENU_TOGGLE.addEventListener("click", () => {
+//     BURGER_MENU.classList.toggle("active")
+//     BURGER_MENU.classList.toggle("inactive")
+// })
 
 addEventListener("resize", () => {
     if (screen.width > 480) {
