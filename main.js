744ec86694c8fb9c0d8debc8114a583c40bbb6da
Techno <meroypaulvincent@gmail.com>
2023-09-26 20:16:10 +0800

    projects section

--- main.js
+++ main.js
@@ -3,8 +3,6 @@ const MENU_TOGGLE = document.querySelectorAll(".toggle")
 
 MENU_TOGGLE.forEach(toggle => {
     toggle.addEventListener("click", (e) => {
-        console.log(toggle, e.currentTarget)
-
         BURGER_MENU.classList.toggle("active")
         BURGER_MENU.classList.toggle("inactive")
     })
